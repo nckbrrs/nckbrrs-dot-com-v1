b70971902c7a4b8d80744b083d79d26b8fce2196
Nick Barrs <nick.barrs@ibm.com>
2021-03-11 23:28:15 -0500

    added color change on hover to footer links

--- src/components/FooterContent/FooterContent.tsx
+++ src/components/FooterContent/FooterContent.tsx
@@ -12,54 +12,60 @@ import ResumeIcon from '../../assets/icons/Resume';
 import ResumePDF from '../../assets/resume/resume.pdf';
 
 class FooterContent extends React.Component<{}, {}> {
+  spaceBetweenLinks() {
+    return (<span>&nbsp;&nbsp;&nbsp;</span>);
+  }
+
   render() {
     return (
       <div className="bx--row footer-content">
         <div className="bx--col">
-          <a href="https://music.apple.com/us/artist/nick-barrs/1539845933">
-            <div className="link-text">MUSIC</div>
-            <AppleMusicIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://open.spotify.com/artist/2ZXVOSBi1E69IxiGuX8ofU?si=cSD9lEXoTRyGvoWBxbHGWA">
-            <div className="link-text">SPOTIFY</div>
-            <SpotifyIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://www.youtube.com/channel/UC4PnMvHxARiaOhzw-99Q4JA">
-            <div className="link-text">YOUTUBE</div>
-            <YoutubeIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://www.twitter.com/nckbrrs">
-            <div className="link-text">TWITTER</div>
-            <TwitterIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://www.instagram.com/nckbrrs">
-            <div className="link-text">INSTAGRAM</div>
-            <InstagramIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://www.github.com/nckbrrs">
-            <div className="link-text">GITHUB</div>
-            <GithubIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="https://www.linkedin.com/in/nckbrrs">
-            <div className="link-text">LINKEDIN</div>
-            <LinkedinIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href="mailto:nckbrrs@gmail.com">
-            <div className="link-text">CONTACT</div>
-            <EmailIcon/>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;</span>
-          <a href={ResumePDF}>
-            <div className="link-text">RESUME</div>
-            <ResumeIcon/>
-          </a>
+          <div id="links">
+            <a href="https://music.apple.com/us/artist/nick-barrs/1539845933">
+              <div className="link-text">MUSIC</div>
+              <AppleMusicIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://open.spotify.com/artist/2ZXVOSBi1E69IxiGuX8ofU?si=cSD9lEXoTRyGvoWBxbHGWA">
+              <div className="link-text">SPOTIFY</div>
+              <SpotifyIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://www.youtube.com/channel/UC4PnMvHxARiaOhzw-99Q4JA">
+              <div className="link-text">YOUTUBE</div>
+              <YoutubeIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://www.twitter.com/nckbrrs">
+              <div className="link-text">TWITTER</div>
+              <TwitterIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://www.instagram.com/nckbrrs">
+              <div className="link-text">INSTAGRAM</div>
+              <InstagramIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://www.github.com/nckbrrs">
+              <div className="link-text">GITHUB</div>
+              <GithubIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="https://www.linkedin.com/in/nckbrrs">
+              <div className="link-text">LINKEDIN</div>
+              <LinkedinIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href="mailto:nckbrrs@gmail.com">
+              <div className="link-text">CONTACT</div>
+              <EmailIcon/>
+            </a>
+            {this.spaceBetweenLinks()}
+            <a href={ResumePDF}>
+              <div className="link-text">RESUME</div>
+              <ResumeIcon/>
+            </a>
+          </div>
         </div>
       </div>
     );
