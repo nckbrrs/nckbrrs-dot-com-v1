1f50ae52c6c6a923be578fc0e215287c6b446cd4
Nick Barrs <nick.barrs@ibm.com>
2021-04-02 12:08:33 -0400

    updated icon svgs (clip-path -> clipPath), also made Main's responsiveness using React state instead of @media scss

--- src/assets/icons/Email.tsx
+++ src/assets/icons/Email.tsx
@@ -6,7 +6,7 @@ export default function EmailIcon() {
 			<circle r="32" cx="32" cy="32"/>
 		</clipPath>
 
-		<path clip-path="url(#clipCircle)" d="M41.1,25H22.9l9.1,7.1L41.1,25z M44,26.6l-12,9.3l-12-9.3V39h24V26.6z M0,0v64h64V0H0z M47,42H17V22h30V42z"/>
+		<path clipPath="url(#clipCircle)" d="M41.1,25H22.9l9.1,7.1L41.1,25z M44,26.6l-12,9.3l-12-9.3V39h24V26.6z M0,0v64h64V0H0z M47,42H17V22h30V42z"/>
 
 		</svg>
 	);
