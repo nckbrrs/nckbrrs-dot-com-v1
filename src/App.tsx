4948c89015c474aa4f9b39b2c907f93f1d00dd11
Nick Barrs <nick.barrs@ibm.com>
2021-05-07 09:39:13 -0400

    forgot to comment out Beta import, caused warning

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./views/Home";
-import Beta from "./views/Beta";
+//import Beta from "./views/Beta";
 
 class App extends React.Component<{}, {}> {
     render() {
